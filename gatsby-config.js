a89eeb39aa8d865aef705f6740f1b43684319eb5
Jim Ettig <42248505+jettig29@users.noreply.github.com>
2020-03-19 22:00:51 -0700

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-service-relief`,
+      options: {
+        authorName: `@amber1ey`,
+        authorLink: `https://twitter.com/amber1ey`,
+        city: `Austin`,
+        state: `Texas`,
+        submitForm: `https://docs.google.com/forms/d/e/1FAIpQLSdWyi2VJbpzrG4Cudn-ULaoPtBbmWHtZaCWwAyblP6WkJEf9g/viewform?embedded=true`,
+      },
+    },
+  ],
+}
